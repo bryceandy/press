a604d6ec217925704258acec9c81c765ab193d3c
bryceandy <bryceandy@rocketmail.com>
2020-05-25 22:41:45 +0300

    Refactor method

--- src/MarkdownParser.php
+++ src/MarkdownParser.php
@@ -14,8 +14,6 @@ class MarkdownParser
      */
     public static function parse(String $string)
     {
-        $parsedown = new Parsedown();
-
-        return $parsedown->text($string);
+        return Parsedown::instance()->text($string);
     }
 }
