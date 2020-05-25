2eead2e668445ef4b1bd1679d71ad2d7aed4f4c0
bryceandy <bryceandy@rocketmail.com>
2020-05-26 01:24:27 +0300

    Test whether the head and body can be split in the markdown

---
+++ tests/Feature/PressFileParserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bryceandy\Press\Tests;
+
+use Bryceandy\Press\PressFileParser;
+use Orchestra\Testbench\TestCase;
+
+class PressFileParserTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function the_head_and_body_gets_split()
+    {
+        $pressFileParser = (new PressFileParser(__DIR__.'/../blogs/MarkFile1.md'));
+
+        $data = $pressFileParser->getData();
+
+        $this->assertStringContainsString('title: My title', $data[1]);
+        $this->assertStringContainsString('description: Description here', $data[1]);
+        $this->assertStringContainsString('Blog post body here', $data[2]);
+    }
+}
