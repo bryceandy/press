04a3296ffc7c357760de901d16fe5679f0fb568a
bryceandy <bryceandy@rocketmail.com>
2020-05-25 22:07:49 +0300

    Test if the markdown can parse a level on header

---
+++ tests/Unit/MarkdownTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bryceandy\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class MarkdownTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function a_level_one_header_can_be_parsed()
+    {
+        $heading = MarkdownParser::parse('# This is a heading');
+
+        $this->assertEquals($heading, '<h1>This is a heading</h1>');
+    }
+}
