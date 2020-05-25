e389cc6353cb8bf17a0d5e9256a3a8fec6b933db
bryceandy <bryceandy@rocketmail.com>
2020-05-25 21:24:25 +0300

    Add a sample test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bryceandy\Press\Test;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A sample test
+     *
+     * @test
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
