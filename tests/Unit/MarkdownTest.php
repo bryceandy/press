8128ebae4d3a70e6bbfdfee8b5705c617ce0965c
bryceandy <bryceandy@rocketmail.com>
2020-05-25 22:34:22 +0300

    Add a unit test for level 2 headings

--- tests/Unit/MarkdownTest.php
+++ tests/Unit/MarkdownTest.php
@@ -16,4 +16,14 @@ class MarkdownTest extends TestCase
 
         $this->assertEquals($heading, '<h1>This is a heading</h1>');
     }
+
+    /**
+     * @test
+     */
+    public function a_level_two_header_can_be_parsed()
+    {
+        $heading = MarkdownParser::parse('## This is another heading');
+
+        $this->assertEquals($heading, '<h2>This is another heading</h2>');
+    }
 }
