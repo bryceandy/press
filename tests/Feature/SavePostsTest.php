66b3f9379e2a1ca230a999ac3274d149eaa06208
bryceandy <bryceandy@rocketmail.com>
2020-05-27 02:42:48 +0300

    Test a factory can save data

---
+++ tests/Feature/SavePostsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Bryceandy\Press\Tests\Feature;
+
+use Bryceandy\Press\Post;
+use Bryceandy\Press\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SavePostsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function a_post_can_be_created_with_a_factory()
+    {
+        factory(Post::class)->create();
+
+        $this->assertCount(1, Post::all());
+    }
+}
