29b5608fc734f1b9eaccc599e6518d46bd1b5121
bryceandy <bryceandy@rocketmail.com>
2020-05-26 23:59:52 +0300

    Add field classes for the remaining fields

---
+++ src/Fields/Description.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bryceandy\Press\Fields;
+
+use Bryceandy\Press\Fields\Contracts\FieldContract;
+
+class Description extends FieldContract
+{
+    //
+}
---
+++ src/Fields/Title.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bryceandy\Press\Fields;
+
+use Bryceandy\Press\Fields\Contracts\FieldContract;
+
+class Title extends FieldContract
+{
+    //
+}
