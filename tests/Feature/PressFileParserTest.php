1619424887fc45fc0f02998801b217a6f350baa5
bryceandy <bryceandy@rocketmail.com>
2020-05-26 16:36:53 +0300

    Choose a random object to be parsed

--- tests/Feature/PressFileParserTest.php
+++ tests/Feature/PressFileParserTest.php
@@ -13,7 +13,14 @@ class PressFileParserTest extends TestCase
     {
         parent::setUp();
 
-        $pressFileParser = (new PressFileParser(__DIR__.'/../blogs/MarkFile1.md'));
+        $contentToBeParsed = [
+            __DIR__.'/../blogs/MarkFile1.md',
+            "---\ntitle: My title\ndescription: Description here\n---\n\n# Heading\n\nBlog post body here",
+        ];
+
+        $randomIndex = array_rand($contentToBeParsed);
+
+        $pressFileParser = (new PressFileParser($contentToBeParsed[$randomIndex]));
 
         $this->data = $pressFileParser->getData();
     }
@@ -28,19 +35,6 @@ class PressFileParserTest extends TestCase
         $this->assertStringContainsString('description: Description here', $this->data[1]);
         
         $this->assertStringContainsString('Blog post body here', $this->data[2]);
-    }
-
-    /**
-     * @test
-     */
-    public function a_string_can_also_be_used_instead()
-    {
-        $pressFileParser = (new PressFileParser(
-            "---\ntitle: My title\ndescription: Description here\n---\n\n# Heading\n\nBlog post body here"
-        ));
-
-        $data = $pressFileParser->getData();
-        $this->assertStringContainsString('title: My title', $data[1]);
     }
 
     /**
