4dc05608f2120a91c1680c54e5e6524baf884886
bryceandy <bryceandy@rocketmail.com>
2020-05-27 01:40:28 +0300

    Create base service provider and register it in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,14 @@
 
 namespace Bryceandy\Press\Tests;
 
+use Bryceandy\Press\PressBaseServiceProvider;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-
+    protected function getPackageProviders($app)
+    {
+        return [
+            PressBaseServiceProvider::class,
+        ];
+    }
 }
