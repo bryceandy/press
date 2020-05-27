6a49fa1382e8a1d0feee350f7f0c3c2d0f7b6307
bryceandy <bryceandy@rocketmail.com>
2020-05-27 03:49:52 +0300

    Update command to process blog posts

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -2,7 +2,11 @@
 
 namespace Bryceandy\Press\Console;
 
+use Bryceandy\Press\Post;
+use Bryceandy\Press\PressFileParser;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class ProcessCommand extends Command
 {
@@ -12,6 +16,25 @@ class ProcessCommand extends Command
 
     public function handle()
     {
-        //
+        $this->info('Processing posts...');
+
+        // Fetch all posts
+        $files = File::files('blogs');
+
+        // Process each file
+        collect($files)->map(function ($file) {
+
+            $post = (new PressFileParser($file->getPathName()))->getData();
+
+            Post::create([
+                'identifier' => Str::random(),
+                'slug' => Str::slug($post['title']),
+                'title' => $post['title'],
+                'body' => $post['body'],
+                'extra' => $post['extra'] ?? [],
+            ]);
+        });
+
+        $this->info('Posts updated successfully!');
     }
 }
