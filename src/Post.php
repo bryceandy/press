ca876a29e71d99a14cb32443ab8a5ef609e10f0d
bryceandy <bryceandy@rocketmail.com>
2020-05-28 19:08:22 +0300

    Enable selecting extra properties

--- src/Post.php
+++ src/Post.php
@@ -8,7 +8,21 @@ class Post extends Model
 {
     protected $guarded = [];
 
+    /**
+     * Attributes that should be casted to a datatype
+     * @return array
+     */
     protected $casts = [
         'extra' => 'array',
     ];
+
+    /**
+     * Enable a user to select an extra field without decoding
+     * 
+     * @return string|null
+     */
+    public function extra($field)
+    {
+        return optional(json_decode($this->extra))->$field;
+    }
 }
