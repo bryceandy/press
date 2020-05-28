2d7cc51dc65eff53d5cd48c65aed992ab322b562
bryceandy <bryceandy@rocketmail.com>
2020-05-28 18:26:06 +0300

    Adds the extra properties that were defined by the user

--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -3,10 +3,16 @@
 namespace Bryceandy\Press\Repositories;
 
 use Bryceandy\Press\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class PostRepository
 {
+    /**
+     * Save or update a post
+     *
+     * @param $post
+     */
     public function save($post)
     {
         Post::updateOrCreate([
@@ -16,7 +22,22 @@ class PostRepository
             'slug' => Str::slug($post['title']),
             'title' => $post['title'],
             'body' => $post['body'],
-            'extra' => $post['extra'] ?? json_encode([]),
+            'extra' => $this->extra($post),
         ]);
+    }
+
+    /**
+     * Adds the extra properties that were defined by the user
+     * 
+     * @param $post
+     * @return false|string
+     */
+    private function extra($post)
+    {
+        $extra = (array) json_decode($post['extra'] ?? '[]');
+
+        $attributes = Arr::except($post, ['title', 'body', 'identifier']);
+
+        return json_encode(array_merge($extra, $attributes));
     }
 }
